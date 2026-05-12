fd361650d2b8b86fc673ccecf71f45023b72824e
Noura Gamil <ngamil025g@gmail.com>
2026-05-12 19:21:05 +0300

    AddingBookingaftermigration

---
+++ Ticket_booking_online_system/DAL/Context/Migrations/20260512162027_updatingSericeTobeUNNULLABLE.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ticket_booking_online_system.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatingSericeTobeUNNULLABLE : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookings_Services_ServiceID",
+                table: "Bookings");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Status",
+                table: "Bookings",
+                type: "int",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ServiceID",
+                table: "Bookings",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_Services_ServiceID",
+                table: "Bookings",
+                column: "ServiceID",
+                principalTable: "Services",
+                principalColumn: "ServiceID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookings_Services_ServiceID",
+                table: "Bookings");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "Bookings",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ServiceID",
+                table: "Bookings",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_Services_ServiceID",
+                table: "Bookings",
+                column: "ServiceID",
+                principalTable: "Services",
+                principalColumn: "ServiceID");
+        }
+    }
+}
