797a487606988742127d8bed7c73237c0d5f1a00
alaaessam <ae321611@gmail.com>
2026-05-15 01:44:16 +0300

    add identity and roles

---
+++ Ticket_booking_online_system/Ticket_booking_online_system/Seed/IdentitySeeder.cs
@@ -0,0 +1,44 @@
+﻿using DAL.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace Ticket_booking_online_system.Seed
+{
+    public class IdentitySeeder
+    {
+        public static async Task SeedAdminAsync(IServiceProvider serviceProvider)
+        {
+            var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            string adminEmail = "admin@test.com";
+            string adminPassword = "Admin@123";
+
+            if (!await roleManager.RoleExistsAsync("Admin"))
+            {
+                await roleManager.CreateAsync(new IdentityRole("Admin"));
+            }
+
+            var adminUser = await userManager.FindByEmailAsync(adminEmail);
+
+            if (adminUser == null)
+            {
+                var admin = new ApplicationUser
+                {
+                    UserName = adminEmail,
+                    Email = adminEmail,
+                    EmailConfirmed = true,
+                    Name = "Admin User",
+                    Passport_num = "A12345678",
+
+                };
+
+                var result = await userManager.CreateAsync(admin, adminPassword);
+
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(admin, "Admin");
+                }
+            }
+        }
+    }
+}
